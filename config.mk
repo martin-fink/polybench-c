1c4dcd8eff1948a594f82e70d99a4cf709f5f344
Martin Fink <martin@finkmartin.com>
2023-05-22 13:31:37 +0000

    Build sanitized and unsanitized versions of polybench

--- config.mk
+++ config.mk
@@ -1,3 +1,4 @@
 CC=clang
-WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot /scratch/martin/src/wasm/wasi-libc/sysroot -g -march=wasm64-wasi+mem-safety -fsanitize=wasm-memsafety -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks /scratch/martin/src/wasm/llvm-project/wasm_memsafety_rtlib.c
+WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot /scratch/martin/src/wasm/wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks /scratch/martin/src/wasm/llvm-project/wasm_memsafety_rtlib.c
+SAN_FLAGS=-march=wasm64-wasi+mem-safety -fsanitize=wasm-memsafety
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO ${WASM_FLAGS}
--- utilities/makefile-gen.pl
+++ utilities/makefile-gen.pl
@@ -62,6 +62,7 @@ include $configFile
 EXTRA_FLAGS=$extra_flags{$kernel}
 
 $kernel: $kernel.c $kernel.h
+	\${VERBOSE} \${CC} -o $kernel-san.wasm $kernel.c \${CFLAGS} -I. -I$utilityDir $utilityDir/polybench.c \${EXTRA_FLAGS} \${SAN_FLAGS}
 	\${VERBOSE} \${CC} -o $kernel.wasm $kernel.c \${CFLAGS} -I. -I$utilityDir $utilityDir/polybench.c \${EXTRA_FLAGS}
 
 clean:
@@ -81,7 +82,8 @@ open FILE, '>'.$TARGET_DIR.'/config.mk';
 
 print FILE << "EOF";
 CC=clang
-WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot /scratch/martin/src/wasm/wasi-libc/sysroot -g -march=wasm64-wasi+mem-safety -fsanitize=wasm-memsafety -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks /scratch/martin/src/wasm/llvm-project/wasm_memsafety_rtlib.c
+WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot /scratch/martin/src/wasm/wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks /scratch/martin/src/wasm/llvm-project/wasm_memsafety_rtlib.c
+SAN_FLAGS=-march=wasm64-wasi+mem-safety -fsanitize=wasm-memsafety
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO \${WASM_FLAGS}
 EOF
 
