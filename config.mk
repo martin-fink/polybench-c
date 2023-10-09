3b0f8a2d251faabb4ba08fb6033f1217b9434d0b
Martin Fink <martin@finkmartin.com>
2023-10-09 16:21:19 +0000

    Update flags

--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
 CC=clang
-WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot /scratch/martin/src/wasm/wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks /scratch/martin/src/wasm/llvm-project/wasm_memsafety_rtlib.c
+WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot ../wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks ../llvm-project/wasm_memsafety_rtlib.c
 SAN_FLAGS=-mmem-safety -fsanitize=wasm-memsafety
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO ${WASM_FLAGS}
+
