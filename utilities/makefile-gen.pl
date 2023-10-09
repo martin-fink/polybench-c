140827e79a0e5b0eb11406fc847c777cd4843cfe
Martin Fink <martin@finkmartin.com>
2023-10-09 17:07:27 +0000

    Update makefile

--- utilities/makefile-gen.pl
+++ utilities/makefile-gen.pl
@@ -82,7 +82,7 @@ open FILE, '>'.$TARGET_DIR.'/config.mk';
 
 print FILE << "EOF";
 CC=clang
-WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot ../wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks ../llvm-project/wasm_memsafety_rtlib.c
+WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot \$(PWD)/../wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks \$(PWD)/../llvm-project/wasm_memsafety_rtlib.c
 SAN_FLAGS=-mmem-safety -fsanitize=wasm-memsafety
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO \${WASM_FLAGS}
 EOF
