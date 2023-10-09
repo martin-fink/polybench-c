a59420ea8636d28713f7d41f20d54c7be64e2219
Martin Fink <martin@finkmartin.com>
2023-10-09 16:46:48 +0000

    Update makefile

--- config.mk
+++ config.mk
@@ -2,4 +2,3 @@ CC=clang
 WASM_FLAGS=--target=wasm64-unknown-wasi --sysroot ../wasi-libc/sysroot -g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks ../llvm-project/wasm_memsafety_rtlib.c
 SAN_FLAGS=-mmem-safety -fsanitize=wasm-memsafety
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO ${WASM_FLAGS}
-
