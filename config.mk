67d0819c6d37b00074d083206fd5094b4a8c8cb4
Martin Fink <martin@finkmartin.com>
2024-05-13 15:36:05 +0000

    gen makefiles

--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
 CC=clang
-WASM_FLAGS=-g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks -fno-inline
-SAN_FLAGS=-mmem-safety #-fsanitize=wasm-memsafety
+WASM_FLAGS=-g -D_WASI_EMULATED_PROCESS_CLOCKS -lwasi-emulated-process-clocks
+SAN_FLAGS=-mmem-safety -fsanitize=wasm-memsafety
+PTR_AUTH_FLAGS=-mmem-safety -fsanitize=wasm-ptr-auth
 CFLAGS=-O2 -DPOLYBENCH_DUMP_ARRAYS -DPOLYBENCH_USE_C99_PROTO ${WASM_FLAGS}
